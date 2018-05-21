1a57b35ec6109393234c83fc0330d73ab873c41a
Dominic <dominic.zedler.one@gmail.com>
2018-05-22 00:51:25 +0200

    results with floating point can be spoken now

--- action-getDivision-Taschenrechner.py
+++ action-getDivision-Taschenrechner.py
@@ -41,10 +41,15 @@ def action_wrapper(hermes, intentMessage, conf):
     first = int(intentMessage.slots.firstTerm.first().value)
     second = int(intentMessage.slots.secondTerm.first().value)
     if second != 0:
-        calc = first / second
+        calc = first / float(second)
         if str(calc)[-2:] == ".0":
             calc = int(calc)
-        result_sentence = "{} geteilt durch {} ergibt {} .".format(first, second, calc)
+        if "." in str(calc):
+            calc_part1, calc_part2 = str(calc).split(".")
+            result_sentence = "{0} geteilt durch {1} ergibt {2} komma {3} .".format(first, second,
+                                                                                   calc_part1, calc_part2)
+        else:
+            result_sentence = "{} geteilt durch {} ergibt {} .".format(first, second, calc)
     else:
         result_sentence = "Ich kann leider nicht durch Null teilen."
 
