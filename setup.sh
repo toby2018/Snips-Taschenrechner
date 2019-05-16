1e8d40eb7b4260e0b6b431179e146cbacdc5f8dc
Dr. John Zoidberg <27244574+MrJohnZoidberg@users.noreply.github.com>
2019-05-16 22:12:39 +0200

    Setup not quiet anymore

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ then
 
     # Activate the virtual environment and install requirements.
     . $VENV/bin/activate
-    pip3 install -q -r requirements.txt
+    pip3 install -r requirements.txt
 
 else
     >&2 echo "Cannot find Python 3. Please install it."
