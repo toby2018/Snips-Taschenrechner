8d204614c7dc93b4fe309de8412e89f7b6423cc9
toby2018 <36097390+toby2018@users.noreply.github.com>
2019-05-30 07:33:43 +0200

    Update action-getHowAreYou-Taschenrechner.py

--- action-getHowAreYou-Taschenrechner.py
+++ action-getHowAreYou-Taschenrechner.py
@@ -20,11 +20,11 @@ def action_wrapper(hermes, intent_message):
     item = intent_message.slots.family_member.first().value
 
     if item == 'Sabine' or item == 'Steffen':
-        # result_sentence = item + random.choice(responses)
-        result_sentence = 'watt dat watt?'
+        result_sentence = item + random.choice(responses)
+        # result_sentence = 'watt dat watt?'
     else:
-        # result_sentence = random.choice(unavailable)
-        result_sentence = 'keiner will die else'
+        result_sentence = random.choice(unavailable)
+        # result_sentence = 'keiner will die else'
 
     current_session_id = intent_message.session_id
     hermes.publish_end_session(current_session_id, result_sentence)
